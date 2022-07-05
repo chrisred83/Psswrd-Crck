b6d9a847c707d10502f755534b72f2de62baa154
chrisred83 <40906611+chrisred83@users.noreply.github.com>
2022-07-05 16:14:19 -0500

    Changed fileName
    Updated fileName -> wordListPy.txt

--- src/python/perms.py
+++ src/python/perms.py
@@ -3,9 +3,9 @@ from itertools import permutations
 #Save permutations to a list
 #Replace palabraConocida for the word you know or a variation of the same word
 listaPalabras = [''.join(palabra) for palabra in permutations('palabraConocida')]
-#Save list to a previously created file, file is empty but it exists
-#Change C:\Users\Username\path\to\your\file\fileName.extension for the path of your file, keep quotes
-rutaAbsoluta = r"C:\Users\Username\path\to\your\file\fileName.extension"
+#Save list to repo empty file wordListPy.txt, file is empty but it exists
+#Change C:\Users\Username\path\to\your\file\wordList.txt for repo file in your pc, keep quotes
+rutaAbsoluta = r"C:\Users\Username\path\to\your\file\wordListPy.txt"
 permsTxt = open(rutaAbsoluta, 'w')
   for palabra in palabras:
     permsTxt.write("%s\n" % palabra)
